c064121293c88350ec6895881f0f7313ca5c1530
JediTerraqueo <51382028+JediTerraqueo@users.noreply.github.com>
2022-07-27 11:49:19 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.forkjoin;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+        System.out.println(ForkJoinAdd.startForkJoinSum(1_000_000));
+		
+    }
+
+}
